8622f71bb40ecc14b4a95733eafa2606336520ae
Christian Parker <christianparker512@gmail.com>
2021-07-22 14:07:30 -0500

    use of objects and returning values

--- src/App.js
+++ src/App.js
@@ -7,15 +7,27 @@ function App() {
   const age = 50;
   const job = "Software Engineer"
 
+  const mArray = [1,2,3,4]
+
+  const mObj = {
+    name: "Quincy",
+    age: 12
+  }
+
+  const inputPlaceholder = 'Enter Your Details'
+
   const getFullName = (firstName, lastName) => {
     return `${firstName} ${lastName} `
   }
   return (
     <div className="App">
       <h3>Full Name: {getFullName(firstName, lastName)}</h3>
-        <p>Age: {age}</p>
+        <p> Dog's name: {mObj.name}</p>
+        <p>Age: {mObj.age}</p>
         <p>Job: {job}</p>
-      <input placeholder='Enter Data'/>
+      {mArray[0]}
+      <input placeholder={inputPlaceholder}
+      autoComplete />
     </div>
   );
 }
